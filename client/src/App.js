dff0861963dd6ac8d88e0b0aaa6af3b1463f1522
Tyler Austin <tyler@webguyty.com>
2020-10-01 20:09:46 -0700

    reverting back

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
+<<<<<<< HEAD
+=======
+import logo from './logo.svg';
+>>>>>>> 38c1793... react install
 import './App.css';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className='App'>
       <header className='App-header'>
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+>>>>>>> 38c1793... react install
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+<<<<<<< HEAD
           className='App-link'
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'
+=======
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+>>>>>>> 38c1793... react install
         >
           Learn React
         </a>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import App from './App';
+=======
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+>>>>>>> 38c1793... react install
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +14,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+<<<<<<< HEAD
+=======
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+>>>>>>> 38c1793... react install
