907e84a2d7c5f42c2dad0393c1484fd4aa504c66
Tyler Austin <tyler@webguyty.com>
2020-10-01 20:14:06 -0700

    updated server when in production mode

--- server.js
+++ server.js
@@ -3,18 +3,28 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect Database s
+// Connect Database g
 connectDB();
 
 // Init Middlware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('shit'));
+// app.get('/', (req, res) => res.send('shit'));
 
 // Define routes
 // app.use('/aroute', require('./routes/test1'));
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
